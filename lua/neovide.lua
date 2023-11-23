5f7aaca1fb27d51bd7fb9fa7f8478e05e2ff8352
zperk13 <46951331+zperk13@users.noreply.github.com>
2023-11-22 23:43:54 -0600

    Fixed insert mode having cursor animations in neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -8,5 +8,5 @@ if vim.g.neovide then
     vim.g.neovide_confirm_quit = true
     vim.g.neovide_cursor_animation_length = 0.2
     vim.g.neovide_cursor_trail_size = 0.1
-    vim.g.neovide_cursor_animation_in_insert_mode = false
+    vim.g.neovide_cursor_animate_in_insert_mode = false
 end
