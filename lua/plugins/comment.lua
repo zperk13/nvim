78895524e004ab20e8878119f506e2f354837f78
zperk13 <46951331+zperk13@users.noreply.github.com>
2024-01-12 21:18:23 -0600

    fixed indenting

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,6 +1,6 @@
-    return {
+return {
         'numToStr/Comment.nvim',
         event = { "BufReadPost", "BufNewFile" },
         config = true,
         cond = vim.g.cfg_complexity == "full"
-    }
+}
