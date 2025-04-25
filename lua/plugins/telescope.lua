5023853ad352f24ca32383a4047e1aa1668cd566
zperk13 <46951331+zperk13@users.noreply.github.com>
2025-04-25 14:46:22 -0500

    removed telescope-fzf-native build command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,8 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build =
-            'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+            'nvim-telescope/telescope-fzf-native.nvim'
+            -- The build command doesn't want to work for me, so just go to ~/.local/share/nvim/lazy/telescope-fzf-native.nvim and run make.
         },
         "nvim-tree/nvim-web-devicons"
     },
