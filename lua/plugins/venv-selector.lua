7398ddf40ae827abe4b53e8c4b0c5260ef1fa4eb
zperk13 <46951331+zperk13@users.noreply.github.com>
2025-08-30 03:08:29 -0500

    updated plugins and switched venv-selector branch

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -5,7 +5,6 @@ return {
         { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
     lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
     keys = {
         { "<leader>lv", "<cmd>VenvSelect<cr>" },
     },
