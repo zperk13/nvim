8385624720f3e5eeb5563ef1bf3a44adb8b8b277
zperk13 <46951331+zperk13@users.noreply.github.com>
2026-09-21 01:29:41 -0500

    detect nix-shell bash filetype?

--- lua/filetype.lua
+++ lua/filetype.lua
@@ -1,4 +1,18 @@
 local add = vim.filetype.add
-add({extension = {
-    mcfunction = "mcfunction"
-}})
+add({
+    extension = {
+        mcfunction = "mcfunction"
+    },
+    pattern = {
+        -- fallback
+        ['.*'] = {
+            function (path, bufnr)
+                local l01 = vim.api.nvim_buf_get_lines(bufnr, 0, 2, false)
+                if vim.regex("^#!.{-}nix-shell"):match_str(l01[0]) and vim.regex("^#! ?nix-shell -i bash"):match_str(l01[1]) then
+                    return "bash"
+                end
+            end,
+            { priority = -math.huge }
+        }
+    }
+})
