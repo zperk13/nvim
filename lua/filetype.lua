6932ee39eccd63dd265d7557c4d44df32190a3f2
zperk13 <46951331+zperk13@users.noreply.github.com>
2026-09-21 01:32:24 -0500

    fricking 0 vs 1 indexing

--- lua/filetype.lua
+++ lua/filetype.lua
@@ -7,8 +7,8 @@ add({
         -- fallback
         ['.*'] = {
             function (path, bufnr)
-                local l01 = vim.api.nvim_buf_get_lines(bufnr, 0, 2, false)
-                if vim.regex("^#!.{-}nix-shell"):match_str(l01[0]) and vim.regex("^#! ?nix-shell -i bash"):match_str(l01[1]) then
+                local ls = vim.api.nvim_buf_get_lines(bufnr, 0, 2, false)
+                if vim.regex("^#!.{-}nix-shell"):match_str(ls[1]) and vim.regex("^#! ?nix-shell -i bash"):match_str(ls[2]) then
                     return "bash"
                 end
             end,
